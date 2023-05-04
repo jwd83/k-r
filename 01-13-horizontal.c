2fd924390a5beafffc3371dab683989f73ef6512
Jared De Blander <jared0x90@gmail.com>
2023-05-04 05:21:16 -0400

    added more non whitespace filters

--- 01-13-horizontal.c
+++ 01-13-horizontal.c
@@ -3,12 +3,18 @@
 #define OUT 0
 #define IN 1
 #define MAX_WORD_LENGTH 20
+#define NOT_A_TERMINATOR 0
+#define IS_A_TERMINATOR 1
 
 int main()
 {
     char c;
     int word_length = 0;
     int length_count[MAX_WORD_LENGTH];
+    char *terminators = " \n\t.,!=+?;|&-()[]{}<>\"'";
+    int is_terminator = NOT_A_TERMINATOR;
+
+    printf("Send ALT+255 to end input\nPaste Input:");
 
     // set all counts to 0
     for (int i = 0; i < MAX_WORD_LENGTH; ++i)
@@ -16,18 +22,19 @@ int main()
 
     while ((c = getchar()) != EOF)
     {
-        if (c == ';')
-        {
+        // check if c is a terminator
+        is_terminator = NOT_A_TERMINATOR;
 
-            if (word_length > 0 && word_length < MAX_WORD_LENGTH)
+        for (int i = 0; terminators[i] != '\0'; ++i)
+        {
+            if (c == terminators[i])
             {
-                length_count[word_length]++;
-                word_length = 0;
+                is_terminator = IS_A_TERMINATOR;
+                break;
             }
-            break;
         }
 
-        if (c == ' ' || c == '\n' || c == '\t' || c == '.' || c == ',' || c == '!' || c == '?')
+        if (is_terminator == IS_A_TERMINATOR)
         {
             if (word_length > 0 && word_length < MAX_WORD_LENGTH)
             {
@@ -41,6 +48,12 @@ int main()
         }
     }
 
+    if (word_length > 0 && word_length < MAX_WORD_LENGTH)
+    {
+        length_count[word_length]++;
+        word_length = 0;
+    }
+
     for (int i = 1; i < MAX_WORD_LENGTH; ++i)
     {
         if (length_count[i] == 0)
@@ -50,7 +63,7 @@ int main()
         {
             printf("*");
         }
-        printf("\n");
+        printf("\t%d\n", length_count[i]);
     }
 
     return 0;
